8ac075012fbadd3832bc4062f34c6e6e18dcec64
Kribz1k <85782253+robmjk@users.noreply.github.com>
2023-08-28 15:15:28 +1000

    Particle Explosion Program using SDL in C++
    a cool project involving a particle explosion program that showcases vibrant visual effects. Utilised the SDL graphical library to create this captivating display in C++.

---
+++ Swarm.cpp
@@ -0,0 +1,27 @@
+// swarm cpp source
+
+#include "Swarm.h"
+
+namespace kribsplosion
+{
+	Swarm::Swarm() : lastTime(0) 
+	{
+		m_pParticles = new Particle[NPARTICLES];
+	}
+
+	Swarm::~Swarm() 
+	{
+		delete[] m_pParticles;
+	}
+
+	void Swarm::update(int elapsed) 
+	{
+		int interval = elapsed - lastTime;
+
+		for (int i = 0; i < Swarm::NPARTICLES; i++) 
+		{
+			m_pParticles[i].update(interval);
+		}
+		lastTime = elapsed;
+	}
+}
